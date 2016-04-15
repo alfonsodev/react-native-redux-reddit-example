747ecefddb281da5e482d0f281127f1840ea2e26
Alfosno <fernandez@ghinwa.me>
2016-04-15 15:10:01 +0200

    Android index should be the same as iOS

--- index.android.js
+++ index.android.js
@@ -10,22 +10,11 @@ import React, {
   Text,
   View
 } from 'react-native';
+import Root from './app/containers/Root'
 
 class reddit extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return (<Root />);
   }
 }
 
@@ -36,16 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('reddit', () => reddit);
